d6b4b61df807303e49815e524cb6b42a802741be
Tommaso Passarotto <passarotto13@gmail.com>
2021-11-08 15:20:43 +0100

    pseudocodice

--- js/data.js
+++ js/data.js
@@ -1,114 +1,135 @@
-[
-	{
-		name: 'cat',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'crow',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'dog',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'dove',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'dragon',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'horse',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'hippo',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'fish',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
-	},
-	{
-		name: 'carrot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'apple-alt',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'lemon',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'pepper-hot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
-	},
-	{
-		name: 'user-astronaut',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	},
-	{
-		name: 'user-graduate',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	},
-	{
-		name: 'user-ninja',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	},
-	{
-		name: 'user-secret',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	}
+// 1. creare il contenitore di html in cui inserire le icone
+// 2. creare l'innerHTML dove inserire il codice dell'icona
+	// 2.1 creare un ciclo per mettere in ordine corretto gli elementi degli oggetti
+// 3. creare il css per i contenitori delle icone
+
+
+const icons = [
+
+		{
+			name: 'cat',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'crow',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+		{
+			name: 'dog',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'dove',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'dragon',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'horse',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'hippo',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'fish',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas',
+			color: 'orange'
+		},
+
+		{
+			name: 'carrot',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas',
+			color: 'green'
+		},
+
+		{
+			name: 'apple-alt',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas',
+			color: 'green'
+		},
+
+		{
+			name: 'lemon',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas',
+			color: 'green'
+		},
+
+		{
+			name: 'pepper-hot',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas',
+			color: 'green'
+		},
+
+		{
+			name: 'user-astronaut',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas',
+			color: 'blue'
+		},
+
+		{
+			name: 'user-graduate',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas',
+			color: 'blue'
+		},
+
+		{
+			name: 'user-ninja',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas',
+			color: 'blue'
+		},
+
+		{
+			name: 'user-secret',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas',
+			color: 'blue'
+		}
 ];
